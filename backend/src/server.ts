9cb08ac77e271daafa75c75a5fadd562c897233b
pulcovamon <pulcovamon@gmail.com>
2025-05-04 23:17:37 +0200

    Fix order of app.use

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,9 +6,8 @@ import swaggerDocument from '../swagger.json';
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.use("/api", router);
-
 app.use(express.json());
+app.use("/api", router);
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
