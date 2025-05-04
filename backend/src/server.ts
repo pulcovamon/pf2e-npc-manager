c573652423afe58b8aedfd0b7c326d559457eab5
pulcovamon <pulcovamon@gmail.com>
2025-05-04 23:09:50 +0200

    Add swagger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,16 @@
 import express, { Express } from "express";
 import router from "./routes";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.use("", router);
+app.use("/api", router);
+
+app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
